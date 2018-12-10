570469cb85d995a92b3c15d1b44a990fa17d748c
Miguel Peralvo <miguel.peralvo@gmail.com>
2018-12-10 12:38:31 +0000

    Initial consumer tests with multiple queues.

--- python/new_task.py
+++ python/new_task.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
+import os
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=os.getenv('RABBITMQ_HOST', '127.0.0.1'), port=os.getenv('RABBITMQ_PORT', 5672),
+        virtual_host=os.getenv('RABBITMQ_VHOST', '/'),
+        credentials=pika.credentials.PlainCredentials(
+            username=os.getenv('RABBITMQ_LOGIN', 'guest'),
+            password=os.getenv('RABBITMQ_PASSWORD', 'guest')
+        )
+    ))
+    channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+    channel.queue_declare(queue='datalake_10001', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange='',
-                      routing_key='task_queue',
-                      body=message,
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-print(" [x] Sent %r" % message)
-connection.close()
+    message = ' '.join(sys.argv[1:]) or "Hello World!"
+    channel.basic_publish(exchange='',
+                          routing_key='task_queue',
+                          body=message,
+                          properties=pika.BasicProperties(
+                             delivery_mode=2, # make message persistent
+                          ))
+    print(" [x] Sent %r" % message)
+    connection.close()
--- python/worker_select.py
+++ python/worker_select.py
@@ -10,28 +10,26 @@ def callback(ch, method, properties, body):
     print(" [x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def create_connection_for_queue(queue):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
+def on_open(connection):
+    connection.channel(on_channel_open)
+
+
+def on_channel_open(channel):
+    channel.basic_consume(callback, queue='datalake_10001')
+    channel.basic_consume(callback, queue='datalake_10090')
+
+if __name__ == '__main__':
+    connection = pika.SelectConnection(pika.ConnectionParameters(
         host=os.getenv('RABBITMQ_HOST', '127.0.0.1'), port=os.getenv('RABBITMQ_PORT', 5672),
         virtual_host=os.getenv('RABBITMQ_VHOST', '/'),
         credentials=pika.credentials.PlainCredentials(
             username=os.getenv('RABBITMQ_LOGIN', 'guest'),
             password=os.getenv('RABBITMQ_PASSWORD', 'guest')
         )
-    ))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue, durable=True)
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(callback, queue=queue, arguments={'queue': queue})
+    ), on_open_callback=on_open)
 
-if __name__ == '__main__':
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    queues = ['datalake_10001', 'datalake_10090']
-
-    for queue in queues:
-        channel = create_connection_for_queue(queue)
-        channel.start_consuming()
-
-
-
-
+    try:
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        connection.ioloop.start()
+    except KeyboardInterrupt:
+        connection.close()
