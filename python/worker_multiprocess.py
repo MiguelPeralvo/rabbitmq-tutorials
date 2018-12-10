e92fd6a97503e28cea4ab3193f876f4b08b4792b
Miguel Peralvo <miguel.peralvo@gmail.com>
2018-12-10 13:35:58 +0000

    We have thread-safe multiprocess workers.

--- python/worker_multiprocess.py
+++ python/worker_multiprocess.py
@@ -2,7 +2,7 @@
 import os
 import pika
 import time
-
+import multiprocessing
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
@@ -23,14 +23,34 @@ def create_connection_for_queue(queue):
     channel.queue_declare(queue=queue, durable=True)
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(callback, queue=queue, arguments={'queue': queue})
+    return channel
+
+def consume(queue):
+    print("[*] Creating connection for queue: {}".format(queue))
+    channel = create_connection_for_queue(queue)
+    print("[*] Created connection for queue: {}".format(queue))
+    channel.start_consuming()
 
 if __name__ == '__main__':
-    print(' [*] Waiting for messages. To exit press CTRL+C')
     queues = ['datalake_10001', 'datalake_10090']
+    workers = 10
+    pool = multiprocessing.Pool(processes=workers)
 
-    for queue in queues:
-        channel = create_connection_for_queue(queue)
-        channel.start_consuming()
+    for i in range(0, workers):
+        pool.apply_async(consume, kwds={'queue': queues[i % len(queues)]})
+
+    # Stay alive
+    try:
+        while True:
+            continue
+    except KeyboardInterrupt:
+        print('[*] Exiting...')
+        pool.terminate()
+        pool.join()
+
+    print("[*] Waiting for messages. To exit press CTRL+C'")
+
+
 
 
 
